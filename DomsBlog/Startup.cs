977a16502284c9a0b42929f295096ddd3891230c
Dominic Millar <dominic.millar@xero.com>
2016-11-10 12:25:42 +1100

    Add project files.

---
+++ DomsBlog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DomsBlog.Startup))]
+namespace DomsBlog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
